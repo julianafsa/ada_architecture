60f2dca181a3d8e676fccace01701d030faac5d1
julianafsa <juliana.aquino@f1rst.com.br>
2023-03-19 17:17:45 -0300

    Changing sticker update dto to allow full update.

--- cloud/stickers/src/main/java/br/com/ada/stickers/model/dto/StickerUpdateDTO.java
+++ cloud/stickers/src/main/java/br/com/ada/stickers/model/dto/StickerUpdateDTO.java
@@ -1,8 +1,6 @@
 package br.com.ada.stickers.model.dto;
 
-import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.Digits;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,13 +9,17 @@ import java.math.BigDecimal;
 @Data
 @NoArgsConstructor
 public class StickerUpdateDTO {
+    @NotBlank(message = "Image field is missing or blank.")
     private String image;
 
+    @NotBlank(message = "Description field is missing or blank.")
     private String description;
 
+    @NotBlank(message = "Rarirty field is missing or blank.")
     @Pattern(regexp="^[1-4]{1}")
     private String rarity;
 
+    @NotNull(message = "Price field is missing or blank.")
     @DecimalMin(value = "0.0", inclusive = false)
     @Digits(integer=10, fraction=2)
     private BigDecimal price;
