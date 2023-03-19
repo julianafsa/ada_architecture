145e6012447b00c842539f313a75cf543e1ea128
julianafsa <juliana.aquino@f1rst.com.br>
2023-03-19 06:23:48 -0300

    Adding albuns and stickers projects, main dependencies and application.yml configuration.

---
+++ src/main/java/br/com/ada/ada_architecture/AdaArchitectureApplication.java
@@ -0,0 +1,13 @@
+package br.com.ada.ada_architecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdaArchitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdaArchitectureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/ada/ada_architecture/AdaArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ada.ada_architecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdaArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
