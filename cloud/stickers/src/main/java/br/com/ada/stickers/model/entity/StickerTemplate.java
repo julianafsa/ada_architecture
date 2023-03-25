50520341df0b4932ff076c0b71e8b4be4a4fe1a8
julianafsa <juliana.aquino@f1rst.com.br>
2023-03-25 17:58:00 -0300

    Adding sticker template crud.

---
+++ cloud/stickers/src/main/java/br/com/ada/stickers/model/entity/StickerTemplate.java
@@ -0,0 +1,38 @@
+package br.com.ada.stickers.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Entity
+@Table(name = "sticker_template")
+@NoArgsConstructor
+@AllArgsConstructor
+public class StickerTemplate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "albumTemplateUuid", nullable = false)
+    private String albumTemplateId;
+
+    @Column(name = "number", nullable = false)
+    private int number;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "image", nullable = false)
+    private String image;
+
+    @Column(name = "rarity", nullable = false)
+    private int rarity;
+
+    @Column(name = "sticker_price", nullable = false)
+    private BigDecimal stickerPrice;
+}
