52900121271422f0331084cb39bdda1e9f11b74c
julianafsa <juliana.aquino@f1rst.com.br>
2023-03-29 02:13:21 -0300

    Adding test class for StickerToSellServiceImpl class.

--- cloud/stickers/src/main/java/br/com/ada/stickers/model/dto/StickerToSellUpdateDTO.java
+++ cloud/stickers/src/main/java/br/com/ada/stickers/model/dto/StickerToSellUpdateDTO.java
@@ -1,13 +1,17 @@
 package br.com.ada.stickers.model.dto;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class StickerToSellUpdateDTO {
 //    @NotNull(message = "Sticker field is missing or blank.")
 //    private Sticker sticker;
